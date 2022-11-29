19821b33b801dda2b9dd7670e83ff2e5e51ca844
Dhia-Meniaoui <diaminiaoui@gmail.com>
2022-11-29 23:54:26 +0100

    29/10

---
+++ dhiaServices/fraud/src/main/java/com/dhia/fraud/FraudCheckHistory.java
@@ -0,0 +1,25 @@
+package com.dhia.fraud;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "fraud")
+public class FraudCheckHistory {
+    @Id
+    @SequenceGenerator(
+            name = "fraud_id_sequence",
+            sequenceName = "fraud_id_sequence"
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "fraud_id_sequence"
+    )
+    private Integer id;
+}
